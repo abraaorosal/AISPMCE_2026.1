cc067b05ba7cde02799249eedc3f2eff4cb05d3c
abraaorosal <abraaohenrique.10@gmail.com>
2026-04-04 20:20:11 -0300

    corrige paths do github pages e adiciona favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/AISPMCE_2026.1/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
-});
+}));
